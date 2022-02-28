e8705f6b08a72ec197995434ff6d64168bdacd11
Daniel <tacombel@gmail.com>
2022-02-28 15:35:20 +0100

    Primera aproximacion a cambiar el precio

--- scprime_price_check.py
+++ scprime_price_check.py
@@ -2,22 +2,29 @@ import os
 import requests
 import json
 
-target_price = 3 # $/TB
+target_price = 3.0 # $/TB
+tolerance = 1.0 #difference must be greater than this to change the price
 
 url = 'https://api.coinbase.com/v2/exchange-rates?currency=SCP'
 r = requests.get(url)
 data = json.loads(r.text)
-print(data['data']['rates']['USD'])
-print(data['data']['rates']['EUR'])
+print(f'$/SCP: {data['data']['rates']['USD']}')
+print(f'EUR/SCP: {data['data']['rates']['EUR']}')
 host_v = os.popen('docker exec scprime01 spc host -v').readlines()
 
 n = 1
 for e in host_v:
     if n == 14:
-        collateral = e.split()[1]
+        collateral = float(e.split()[1])
         print(collateral)
     if n == 22:
-        minstorageprice = e.split()[1]
+        minstorageprice = float(e.split()[1])
         print(minstorageprice)
         break
     n += 1
+
+target_scp_price = target_price / data['data']['rates']['USD']
+current_scp_price = minstorageprice / data['data']['rates']['USD']
+
+if abs((current_scp_price / target_scp_price - 1 ) * 100)) > tolerance:
+    print(f'Cambiamos el precio desde {current_scp_price} a {target_scp_price}')
