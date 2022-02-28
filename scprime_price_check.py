ea0911cc76566275a06d184b920f63cb80b796d7
Daniel <tacombel@gmail.com>
2022-02-28 16:18:32 +0100

    General cleaning

--- scprime_price_check.py
+++ scprime_price_check.py
@@ -2,24 +2,25 @@ import os
 import requests
 import json
 
-target_price = 3.01 # $/TB
+# config: update this values to your preferences
+target_price = 3.0 # $/TB
 tolerance = 1.0 # difference must be greater than this % to change the price
+base_cmd = 'docker exec scprime02 spc' # the first part of the command you use to call spc
+# end config
 
 url = 'https://api.coinbase.com/v2/exchange-rates?currency=SCP'
 r = requests.get(url)
 data = json.loads(r.text)
 print(f"$/SCP: {data['data']['rates']['USD']}")
 print(f"EUR/SCP: {data['data']['rates']['EUR']}")
-host_v = os.popen('docker exec scprime02 spc host -v').readlines()
+host_v = os.popen(base_cmd + ' host -v').readlines()
 
 n = 1
 for e in host_v:
     if n == 14:
         collateral = float(e.split()[1])
-        print(collateral)
     if n == 22:
         minstorageprice = float(e.split()[1])
-        print(minstorageprice)
         break
     n += 1
 
@@ -27,10 +28,10 @@ target_scp_price = target_price / float(data['data']['rates']['USD'])
 current_scp_price = minstorageprice
 
 if abs((current_scp_price / target_scp_price - 1 ) * 100) > tolerance:
-    print(f"Cambiamos el precio desde {current_scp_price} a {target_scp_price}")
-    os.system('docker exec scprime02 spc host config minstorageprice ' + str(target_scp_price) + 'SCP')
-    os.system('docker exec scprime02 spc host config collateral ' + str(target_scp_price) + 'SCP')
+    print(f"Changing price from {current_scp_price} to {target_scp_price}")
+    os.system(base_cmd + ' host config minstorageprice ' + str(target_scp_price) + 'SCP')
+    os.system(base_cmd + ' host config collateral ' + str(target_scp_price) + 'SCP')
 else:
-    print(f"El precio está dentro de la tolerancia del {tolerance}%")
+    print(f"The difference in price is less than {tolerance}%")
 
-os.system('docker exec scprime02 spc host -v')
+os.system(base_cmd + ' host -v')
