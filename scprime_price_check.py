08df2f96c90d27282eda173f8845ab01b60c6d6b
Daniel <tacombel@gmail.com>
2022-02-28 16:04:07 +0100

    Primera version funcional

--- scprime_price_check.py
+++ scprime_price_check.py
@@ -2,15 +2,15 @@ import os
 import requests
 import json
 
-target_price = 3.0 # $/TB
-tolerance = 1.0 #difference must be greater than this to change the price
+target_price = 3.01 # $/TB
+tolerance = 1.0 # difference must be greater than this % to change the price
 
 url = 'https://api.coinbase.com/v2/exchange-rates?currency=SCP'
 r = requests.get(url)
 data = json.loads(r.text)
-print(f'$/SCP: {data['data']['rates']['USD']}')
-print(f'EUR/SCP: {data['data']['rates']['EUR']}')
-host_v = os.popen('docker exec scprime01 spc host -v').readlines()
+print(f"$/SCP: {data['data']['rates']['USD']}")
+print(f"EUR/SCP: {data['data']['rates']['EUR']}")
+host_v = os.popen('docker exec scprime02 spc host -v').readlines()
 
 n = 1
 for e in host_v:
@@ -23,8 +23,14 @@ for e in host_v:
         break
     n += 1
 
-target_scp_price = target_price / data['data']['rates']['USD']
-current_scp_price = minstorageprice / data['data']['rates']['USD']
+target_scp_price = target_price / float(data['data']['rates']['USD'])
+current_scp_price = minstorageprice
 
-if abs((current_scp_price / target_scp_price - 1 ) * 100)) > tolerance:
-    print(f'Cambiamos el precio desde {current_scp_price} a {target_scp_price}')
+if abs((current_scp_price / target_scp_price - 1 ) * 100) > tolerance:
+    print(f"Cambiamos el precio desde {current_scp_price} a {target_scp_price}")
+    os.system('docker exec scprime02 spc host config minstorageprice ' + str(target_scp_price) + 'SCP')
+    os.system('docker exec scprime02 spc host config collateral ' + str(target_scp_price) + 'SCP')
+else:
+    print(f"El precio está dentro de la tolerancia del {tolerance}%")
+
+os.system('docker exec scprime02 spc host -v')
