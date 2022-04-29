0d7191e791802803eba76a5355bd27f824b5a505
Daniel <tacombel@gmail.com>
2022-04-29 19:44:10 +0200

    Changed endpoint to Public/Price targets public

--- scprime_price_check.py
+++ scprime_price_check.py
@@ -1,9 +1,30 @@
-# v0.5
+# v0.6
 
 import os
 import sys
-from price import get_price
+import requests
+from requests.structures import CaseInsensitiveDict
+import json
 from config import Config
+
+
+def get_storageprice():
+
+    url = 'https://grafana.scpri.me/api/ds/query'
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+    headers["Content-Type"] = "application/json"
+
+    data = json.dumps({"queries":[{"refId":"A","datasourceId":2,"rawSql":"select to_scp(network.current_xm_maxstorageprice()*4320*1000000000000) as maxprice,\ncase \nwhen round(storageprice.storage_price*10^15/usd_scp_rate/4320/10000000000) > round(scp.storage_price/10000000000) then\n'⇧'\nwhen round(storageprice.storage_price*10^15/usd_scp_rate/4320/10000000000) < round(scp.storage_price/10000000000) then\n'⇩'\nelse '⬄'\nend as expectation,\nto_char(storageprice.storage_price,'990.00')||' USD ⇝ '||\nto_char(to_scp((storageprice.storage_price*10^15/usd_scp_rate/4320)::numeric(38,0)*1000000000000*4320),'990.00')\n||' SCP' as goal\nfrom markets.target_storage_price_scp scp\ncross join \n(select storage_price from markets.storage_price_usd spu\n where valid_since<=unix_now()\n  order by valid_since desc limit 1\n) storageprice\ncross join (select usd as usd_scp_rate from markets.coingecko_simple cs order by last_updated_at desc limit 1) cs1\nwhere valid_since <= unix_now()\norder by valid_since desc limit 1\n;\n","format":"table","intervalMs":1800000,"maxDataPoints":1129}],"range":{"from":"2022-03-30T17:33:34.857Z","to":"2022-04-29T17:33:34.857Z","raw":{"from":"now-30d","to":"now"}},"from":"1648661614857","to":"1651253614857"})
+    r = requests.post(url, data=data, headers=headers)
+    r = r.json()
+    try:
+        storageprice = r['results']['A']['frames'][0]['data']['values'][0][0]
+    except:
+        print("Error processing JSON", flush=True)
+        storageprice = "no data"
+
+    return storageprice
 
 def main():
     host_v = os.popen(Config.base_cmd + ' host -v').readlines()
@@ -13,7 +34,7 @@ def main():
             minstorageprice = e.split()[1]
             break
         n += 1
-    reference_price = get_price()
+    reference_price = get_storageprice()
     if reference_price == 'no data':
         sys.exit()
     target_scp_price = str(round(float(reference_price) * 0.994, 3))
