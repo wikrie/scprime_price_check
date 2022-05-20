b66e1dcba44c8461dbc432a3604c7c8d3265c883
wikrie <github@wikrie.de>
2022-05-20 11:51:59 +0200

    Update config.py
    Update to my settings

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ import os
 
 class Config(object):
     # the first part of the command you use to call spc
-    base_cmd = 'docker exec scprime02 spc'
-    
+    #base_cmd = 'docker exec scprime02 spc'
+    base_cmd = 'spc'
+    base2_cmd = 'spc -a localhost:4290'
